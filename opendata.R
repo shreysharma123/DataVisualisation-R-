56b130567782c5368f2e068c92d1c4bf0728309d
Shrey Sharma <s3696695@student.rmit.edu.au>
2019-02-22 23:47:52 +0530

    code and visualization of the open data

--- opendata.R
+++ opendata.R
@@ -19,3 +19,16 @@ Drug_Trail <- read.spss("NewDrug.sav")
 
 Drug_Trail <- as.data.frame(Drug_Trail) 
 head(Drug_Trail)
+
+##code and visualisation
+
+ggplot(Drug_Trail,mapping= aes(x = reorder(Subject,Treatment))) +   
+geom_line(aes(y = Before_exp_BP, group = Treatment), colour="blue") +    
+geom_point(size=0.9 ,aes(y=Before_exp_BP),colour="blue") +   
+geom_line(aes(y = After_exp_BP,group = Treatment), colour = "red") +    
+geom_point(size=0.9 , aes(y=After_exp_BP),colour="red") +   
+labs(title="Blood Pressure Level Variation for Treatment and Control Group",caption="Source:calcnet.mth.cmich.edu",x="Subjects",y="Blood Pressure(mm Hg)") +theme_stata() +theme(axis.text.x = element_text(angle = 90, vjust=0.5, size = 7),panel.grid.minor = element_blank()) +   
+geom_vline(aes(xintercept=22.5),colour="dark green",linetype="dashed",size=1.0) + 
+annotate (colour="brown",size=3.5,"text", x = 10, y = 115, label = "<----------Control Group--------->") + 
+annotate(colour="brown",size=3.5,"text", x = 40, y = 115, label = "<---------------Trea tment Group-------------->")+annotate("text",x=9,y=106,label="Before Experiment",size=2.75,col="Blue") + 
+annotate("text",x=9,y=86,label="After Experiment",size=2.75,col="Red")
