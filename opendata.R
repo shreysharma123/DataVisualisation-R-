dc248fc9d98108c5f3eee2ec2c4f5ea4e2c5c153
Shrey Sharma <s3696695@student.rmit.edu.au>
2019-02-22 23:32:28 +0530

    installed all the required packages and imported open data.

--- opendata.R
+++ opendata.R
@@ -1,7 +1,19 @@
-getwd()
+install.packages("readxl")
+install.packages("knitr")
+install.packages("ggplot2")
+install.packages("RColorBrewer")
+install.packages("ggthemes")
 
-a=4
-b=6
-a+b
+library(readxl) # Useful for importing excel sheets 
+library(knitr) #Useful for creating nice tables 
+library(ggplot2) # Usefule for Data Visualisation 
+library(RColorBrewer) 
+library(ggthemes) 
 
-a-b
+install.packages("foreign")
+library(foreign)
+## http://calcnet.mth.cmich.edu/org/spss/Prj_New_DrugData.htm  (OPEN DATA SOURCE)
+
+setwd("C:\\Users\\Shrey Sharma\\Downloads") 
+Drug_Trail <- read.spss("NewDrug.sav")
+
